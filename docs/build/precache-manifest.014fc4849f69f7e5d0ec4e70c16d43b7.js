85888b4759d0aa784bafd69cbbcf024ff97d39b5
Adam Hirst <adam@ahirst.com>
2019-02-26 22:02:25 +0000

    Added build to docs

---
+++ docs/build/precache-manifest.014fc4849f69f7e5d0ec4e70c16d43b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c25907bacb2b0116ae87",
+    "url": "/static/css/main.f796121d.chunk.css"
+  },
+  {
+    "revision": "c25907bacb2b0116ae87",
+    "url": "/static/js/main.c25907ba.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "931f43538b49cce7c5f8",
+    "url": "/static/js/2.931f4353.chunk.js"
+  },
+  {
+    "revision": "7ccf0f1f4c35f90a552948fca990510f",
+    "url": "/static/media/Logo.7ccf0f1f.png"
+  },
+  {
+    "revision": "1562c430e9273b2c3c19b70b6b2a428d",
+    "url": "/static/media/adapter.1562c430.svg"
+  },
+  {
+    "revision": "8e2238b88aa34b4efdb5ef09c19c7fd1",
+    "url": "/static/media/help.8e2238b8.svg"
+  },
+  {
+    "revision": "2f023cdf6ef93739cffb1442c98cdd38",
+    "url": "/static/media/settings.2f023cdf.svg"
+  },
+  {
+    "revision": "85365e320b2742deaf869b0168044bd8",
+    "url": "/static/media/info.85365e32.svg"
+  },
+  {
+    "revision": "67da5c2e2e182f039499d8b0f84dc94e",
+    "url": "/index.html"
+  }
+];
