9a6f5bf10149ab35d4c9192cb7826ad6c3e4125d
Adam Hirst <adam@ahirst.com>
2019-02-27 21:12:13 +0000

    Fixed AdapterCard link style

--- extension-site/src/components/AdapterCard/AdapterCard.js
+++ extension-site/src/components/AdapterCard/AdapterCard.js
@@ -2,16 +2,19 @@ import * as React from 'react';
 import styled from 'styled-components';
 import AdapterTag from '../AdapterTag';
 import { Card } from '..';
+import { Link } from 'react-router-dom';
 
 class AdapterCard extends React.Component {
     render() {
         return (
             <CardContainer>
-                <Card>
-                    <Title>{this.props.title}</Title>
-                    <Subtitle>{this.props.subtitle}</Subtitle>
-                    {this.props.tags.map((tag, index) => <AdapterTag key={`label-${index}`} label={tag} startColor="#9277EE" endColor="#874AE2" />)}
-                </Card>
+                <Link to={this.props.link || ""}>
+                    <Card>
+                        <Title>{this.props.title}</Title>
+                        <Subtitle>{this.props.subtitle}</Subtitle>
+                        {this.props.tags.map((tag, index) => <AdapterTag key={`label-${index}`} label={tag} startColor="#9277EE" endColor="#874AE2" />)}
+                    </Card>
+                </Link>
             </CardContainer>
         );
     }
@@ -24,6 +27,7 @@ margin: 10px;
 
 const Title = styled.div` 
 font-size: 20px;
+color: #000;
 `;
 
 const Subtitle = styled.div` 
