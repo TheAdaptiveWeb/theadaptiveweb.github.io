57a766c53d099e7135b5b9bd2d1a45bcbaf39c5f
Adam Hirst <adam@ahirst.com>
2019-02-27 10:36:42 +0000

    [Fixes #5] Fixed logo path

--- extension-site/src/App.js
+++ extension-site/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Sidebar, Pager } from './components';
+import { Sidebar, Pager, Logo } from './components';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import PreloadLogo from './components/PreloadLogo';
 import { AdapterPage, HelpPage, SettingsPage, InfoPage } from './views';
+import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <>
+          <LogoDiv>
+            <Logo />
+          </LogoDiv>
           <Sidebar />
           <Pager>
             <Route path="/adapters" component={(props) => {
@@ -30,5 +35,28 @@ class App extends Component {
     );
   }
 }
+
+let xOffset = (window.innerWidth / 2) - 200;
+let yOffset = (window.innerHeight / 2) - 57;
+
+const LogoAnimation = keyframes`
+0% {
+  transform: translate(${xOffset}px, ${yOffset}px) rotate(0deg) scale(0);
+}
+50% {
+  transform: translate(${xOffset}px, ${yOffset}px) rotate(360deg) scale(1);
+}
+100% {
+  transform: translate(0, 0);
+}
+`;
+
+const LogoDiv = styled.div`
+animation: ${LogoAnimation} 2s;
+z-index: 1000;
+position: fixed;
+top: 20px;
+left: 163px;
+`;
 
 export default App;
--- extension-site/src/components/Masthead/Masthead.js
+++ extension-site/src/components/Masthead/Masthead.js
@@ -7,9 +7,6 @@ class Masthead extends React.Component {
     render() {
         return (
             <MastheadDiv>
-                <LogoDiv>
-                    <Logo />
-                </LogoDiv>
                 <Title>The Adaptive Web</Title>
                 <Subtitle>Adaptive Web Accessibility Toolkit</Subtitle>
             </MastheadDiv>
@@ -22,29 +19,9 @@ padding: 20px;
 text-align: center;
 `;
 
-let xOffset = (window.innerWidth / 2) - (200 - 37) + 405;
-let yOffset = (window.innerHeight / 2) - (20);
-
-const LogoAnimation = keyframes`
-0% {
-  transform: translate(${xOffset}px, ${yOffset}px) rotate(0deg) scale(0);
-}
-50% {
-  transform: translate(${xOffset}px, ${yOffset}px) rotate(360deg) scale(1);
-}
-100% {
-  transform: translate(0, 0);
-}
-`;
-
-const LogoDiv = styled.div`
-animation: ${LogoAnimation} 2s;
-z-index: 1000;
-`;
-
 const Title = styled.div`
 font-size: 32px;
-margin-top:10px;
+margin-top:85px;
 `;
 
 const Subtitle = styled.div`
