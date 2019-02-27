bfddc1b270b7e466bf90e51fb2d29ead19919364
Adam Hirst <adam@ahirst.com>
2019-02-27 15:43:53 +0000

    Added AdapterModal route

--- extension-site/src/views/AdapterPage/AdapterPage.js
+++ extension-site/src/views/AdapterPage/AdapterPage.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 
 import { Section, AdapterCard, Page, CardList, Modal } from '../../components';
+import { Route } from 'react-router-dom';
 
 class AdapterPage extends React.Component {
     render() {
         let tags = ['vision', 'test', 'test2'];
         return (<Page title="Adapters" subtitle="Install and configure adapters from this page. Select an adapter for more info.">
+            <Route path="/adapters/:id" component={(props) => {
+              return <Modal>{props.match.params.id}</Modal>
+            }}/>
             <Section title="Installed">
                 <CardList>
                     <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
@@ -32,9 +36,6 @@ class AdapterPage extends React.Component {
                     <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
                 </CardList>
             </Section>
-            <Modal>
-                Testing
-            </Modal>
         </Page>);
     }
 }
