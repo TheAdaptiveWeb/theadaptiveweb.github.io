20ef900f6c934e1f81e307cd234d3c9937522dd0
Adam Hirst <adam@ahirst.com>
2019-02-27 21:11:59 +0000

    Added modal animations

--- extension-site/src/components/Modal/Modal.js
+++ extension-site/src/components/Modal/Modal.js
@@ -1,37 +1,67 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 import { Card } from '..';
 
 class Modal extends React.Component {
     render() {
         return (
-            <ModalBackground>
+            <ModalContainer>
                 <CardContainer>
                     <Card>
                         {this.props.children}
                     </Card>
                 </CardContainer>
-            </ModalBackground>
+                <ModalBackground />
+            </ModalContainer>
         );
     }
 }
 
-const ModalBackground = styled.div`
+const ModalContainer = styled.div`
+top: 0;
+left: 400px;
+bottom: 0;
+right: 0;
+position: fixed;
 display: flex;
 justify-content: center;
 align-items: center;
+`;
+
+const ModalBgFrames = keyframes`
+0% {
+    opacity: 0;
+}
+100% {
+    opacity: 1;
+`;
+
+const ModalBackground = styled.div`
 background: linear-gradient(45deg, rgba(119, 188, 238, 0.77) 0%, rgba(74, 144, 226, 0.77) 100%);
 position: fixed;
 top: 0;
 left: 400px;
 right: 0;
 bottom: 0;
+animation: ${ModalBgFrames} 0.5s ease-in-out;
+`;
+
+const CardKeyFrames = keyframes`
+0% {
+    transform: scale(0);
+}
+100% {
+    transform: scale(1);
+}
 `;
 
 const CardContainer = styled.div`
 max-width: 500px;
 width: 100%;
 padding: 20px;
+animation: ${CardKeyFrames} 0.5s ease-in-out;
+z-index: 100;
 `;
 
 
