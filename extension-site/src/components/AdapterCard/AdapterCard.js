104465791cf0461d5d3afeb20cf9b1f725ad85b9
Adam Hirst <adam@ahirst.com>
2019-02-27 10:55:21 +0000

    Changed AdapterCard to use Card component

--- extension-site/src/components/AdapterCard/AdapterCard.js
+++ extension-site/src/components/AdapterCard/AdapterCard.js
@@ -1,24 +1,23 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import AdapterTag from '../AdapterTag';
+import { Card } from '..';
 
 class AdapterCard extends React.Component {
     render() {
         return (
-            <Card>
-                <Title>{this.props.title}</Title>
-                <Subtitle>{this.props.subtitle}</Subtitle>
-                {this.props.tags.map((tag, index) => <AdapterTag key={`label-${index}`} label={tag} startColor="#9277EE" endColor="#874AE2" />)}
-            </Card>
+            <CardContainer>
+                <Card>
+                    <Title>{this.props.title}</Title>
+                    <Subtitle>{this.props.subtitle}</Subtitle>
+                    {this.props.tags.map((tag, index) => <AdapterTag key={`label-${index}`} label={tag} startColor="#9277EE" endColor="#874AE2" />)}
+                </Card>
+            </CardContainer>
         );
     }
 }
 
-const Card = styled.div`
-background-color: #fff;
-box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5);
-border-radius: 3px;
-padding: 20px 15px;
+const CardContainer = styled.div`
 flex-grow: 1;
 margin: 10px;
 `;
