e37e39f87ffd5811c0c205cd234a8adec8d176de
Adam Hirst <adam@ahirst.com>
2019-02-28 15:33:02 +0000

    Abstracted mocking

--- extension-site/src/views/AdapterPage/AdapterPage.js
+++ extension-site/src/views/AdapterPage/AdapterPage.js
@@ -6,41 +6,39 @@ import styled from 'styled-components';
 
 class AdapterPage extends React.Component {
     render() {
-        let tags = ['vision', 'test', 'test2'];
+        // Mocking only - should occur in constructor
+        let subtitle = 'Spoken audio description for web videos.';
+        let adapters = [];
+        for (let i = 1; i < 22; i++) adapters.push({ 
+            id: `adapter${i}`, 
+            title: `Adapter ${i}`, 
+            subtitle, 
+            tags: [ `tag${i}`, `tag${i+1}`, `tag${i+2}` ] });
+        let installedAdapters = [];
+        for (let i = 1; i < 7; i++) installedAdapters.push(`adapter${i}`);
+
+        let installed = adapters.filter(x => installedAdapters.indexOf(x.id) !== -1);
+        let available = adapters.filter(x => installedAdapters.indexOf(x.id) === -1);
+
         return (<Page title="Adapters" subtitle="Install and configure adapters from this page. Select an adapter for more info.">
             <Route path="/adapters/:id" render={(props) => {
+                let { title, subtitle, tags } = adapters.find(x => x.id == props.match.params.id);
                 return (
                     <Modal>
-                        <Title>Video Audio Description</Title>
-                        <Subtitle>Spoken audio description for web videos.</Subtitle>
+                        <Title>{title}</Title>
+                        <Subtitle>{subtitle}</Subtitle>
                         {tags.map((tag, index) => <AdapterTag key={`label-${index}`} label={tag} startColor="#9277EE" endColor="#874AE2" />)}
                     </Modal>
                 );
             }}/>
             <Section title="Installed">
                 <CardList>
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} link="/adapters/xyz" />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
+                    {installed.map(adapter => <AdapterCard title={adapter.title} subtitle={adapter.subtitle} tags={adapter.tags} link={"/adapters/" + adapter.id} />)}
                 </CardList>
             </Section>
             <Section title="Available">
                 <CardList>
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
-                    <AdapterCard title="Video Audio Description" subtitle="Spoken audio description for web videos." tags={tags} />
+                    {available.map(adapter => <AdapterCard title={adapter.title} subtitle={adapter.subtitle} tags={adapter.tags} link={"/adapters/" + adapter.id} />)}
                 </CardList>
             </Section>
         </Page>);
