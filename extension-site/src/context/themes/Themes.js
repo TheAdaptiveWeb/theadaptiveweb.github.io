f50468e68babc048bb5601418e25197cd6afd25a
Adam Hirst <adam@ahirst.com>
2019-02-28 20:46:55 +0000

    Added themes

---
+++ extension-site/src/context/themes/Themes.js
@@ -0,0 +1,32 @@
+
+export const DefaultTheme = {
+    background: '#fafbfc',
+    paper: '#fff',
+    accent: {
+        light: '#7db9e8',
+        normal: '#62A8E8',
+        dark: '#4a90e2'
+    },
+    text: {
+        primary: '#000',
+        secondary: '#4A4A4A',
+    },
+    menuItem: {
+        active: '#fff',
+        inactive: '#000'
+    }
+}
+
+export const DarkTheme = {
+    background: '#1c1c1c',
+    paper: '#292929',
+    accent: DefaultTheme.accent,
+    text: {
+        primary: '#fff',
+        secondary: '#b3b3b3',
+    },
+    menuItem: {
+        active: '#fff',
+        inactive: '#fff'
+    }
+}
