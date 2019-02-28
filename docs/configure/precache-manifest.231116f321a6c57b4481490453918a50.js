bb16b2768a3e32d42dd91c76b9005ead8d3597fc
Adam Hirst <adam@ahirst.com>
2019-02-28 15:33:16 +0000

    Added build to GH pages

---
+++ docs/configure/precache-manifest.231116f321a6c57b4481490453918a50.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d79eaa69678892b0544",
+    "url": "/configure/static/css/main.cd98d195.chunk.css"
+  },
+  {
+    "revision": "0d79eaa69678892b0544",
+    "url": "/configure/static/js/main.0d79eaa6.chunk.js"
+  },
+  {
+    "revision": "481fbd5720b0a3e62079",
+    "url": "/configure/static/js/runtime~main.481fbd57.js"
+  },
+  {
+    "revision": "2229534098bbf6864e3f",
+    "url": "/configure/static/js/2.22295340.chunk.js"
+  },
+  {
+    "revision": "7ccf0f1f4c35f90a552948fca990510f",
+    "url": "/configure/static/media/Logo.7ccf0f1f.png"
+  },
+  {
+    "revision": "1562c430e9273b2c3c19b70b6b2a428d",
+    "url": "/configure/static/media/adapter.1562c430.svg"
+  },
+  {
+    "revision": "8e2238b88aa34b4efdb5ef09c19c7fd1",
+    "url": "/configure/static/media/help.8e2238b8.svg"
+  },
+  {
+    "revision": "2f023cdf6ef93739cffb1442c98cdd38",
+    "url": "/configure/static/media/settings.2f023cdf.svg"
+  },
+  {
+    "revision": "85365e320b2742deaf869b0168044bd8",
+    "url": "/configure/static/media/info.85365e32.svg"
+  },
+  {
+    "revision": "24b458f453aec2b58a3ca7ee89797e84",
+    "url": "/configure/index.html"
+  }
+];
--- docs/configure/service-worker.js
+++ docs/configure/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.014fc4849f69f7e5d0ec4e70c16d43b7.js"
+  "/configure/precache-manifest.231116f321a6c57b4481490453918a50.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/configure/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
