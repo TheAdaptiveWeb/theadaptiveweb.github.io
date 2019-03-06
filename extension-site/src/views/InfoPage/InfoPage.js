40fcc99cfce2d10fae4917d19c2e21fbd60bfa6c
Adam Hirst <adam@ahirst.com>
2019-03-06 19:43:11 +0000

    Added adapters to context

--- extension-site/src/views/InfoPage/InfoPage.js
+++ extension-site/src/views/InfoPage/InfoPage.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 
-import { Page } from '../../components';
+import { Page, Section, Card } from '../../components';
 
 class InfoPage extends React.Component {
     render() {
         return (<Page title="Info">
-            
+            <Section>
+                <Card>
+                    The Adaptive Web is a plugin that adds enhancements and fixes problems with web pages. This makes websites more accessible, and adapts them to suit your needs.
+                </Card>
+            </Section>
         </Page>);
     }
 }
