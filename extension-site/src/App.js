4233b739a5ddef142160e9496f372fa245e36871
Adam Hirst <adam@ahirst.com>
2019-03-18 21:31:16 +0000

    Added license info to code

--- extension-site/src/App.js
+++ extension-site/src/App.js
@@ -1,3 +1,17 @@
+/**
+ *  Copyright 2019 The Adaptive Web. All Rights Reserved.
+ * 
+ *  Licensed under the Mozilla Public License 2.0 (the "License"). 
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *  
+ *      https://www.mozilla.org/en-US/MPL/2.0/
+ *  
+ *  or in the "license" file accompanying this file. This file is distributed 
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ *  express or implied. See the License for the specific language governing 
+ *  permissions and limitations under the License.
+ */
 import React, { Component } from 'react';
 import { Sidebar, Routes, AnimatedLogo } from './components';
 import { HashRouter as Router } from 'react-router-dom';
@@ -64,7 +78,7 @@ class App extends Component {
         globalOptions: this.state.config, 
         updateGlobalOptions: this.updateGlobalOptions.bind(this), 
         installedAdapters: this.state.installedAdapters }}>
-        
+
         <ThemeProvider theme={Themes[this.state.config.theme].theme}>
           <>
           <GlobalStyle />
--- extension-site/src/components/Sidebar/Sidebar.js
+++ extension-site/src/components/Sidebar/Sidebar.js
@@ -1,3 +1,17 @@
+/**
+ *  Copyright 2019 The Adaptive Web. All Rights Reserved.
+ * 
+ *  Licensed under the Mozilla Public License 2.0 (the "License"). 
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *  
+ *      https://www.mozilla.org/en-US/MPL/2.0/
+ *  
+ *  or in the "license" file accompanying this file. This file is distributed 
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ *  express or implied. See the License for the specific language governing 
+ *  permissions and limitations under the License.
+ */
 import * as React from 'react';
 import { Masthead, Menu, MenuItem } from '../';
 
